3957e6abbb15aab17a5cc65c94384b4a223ee008
pyb0924 <pyb0924@sjtu.edu.cn>
2020-02-15 21:17:19 +0800

    first commit

--- code/Region_Estimation.py
+++ code/Region_Estimation.py
@@ -1,6 +1,12 @@
 import numpy as np
 import pandas as pd
-W = np.array([[15, 10, 3, 5, 0, 0], [-3, 10, 0, 0, 5, 0], [0, 10, 0, 0, 0, 5]])
+import matplotlib.pyplot as plt
+W = np.array([
+    [15, 10, 3, 5, 0, 0],
+    [-3, 10, 0, 0, 5, 0],
+    [0, 10, 0, 0, 0, 5]
+])
+
 df = pd.read_excel("data/Region_Estimation.xlsx", sheet_name="Sheet1", index_col="Region")
 print(df)
 
@@ -8,3 +14,7 @@ data = df.values
 
 result = pd.DataFrame((W @ data.T).T,index=df.index,columns=["W","r_c","r_p"])
 print(result)
+result.plot.bar()
+plt.xticks(rotation=45)
+plt.savefig("Region_Estimation.png")
+plt.show()
