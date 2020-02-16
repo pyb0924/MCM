8b947e2efefce50a79141d5df59b1ad127d63a13
pyb0924 <pyb0924@sjtu.edu.cn>
2020-02-16 16:26:25 +0800

    add factor:population

--- code/Region_Estimation.py
+++ code/Region_Estimation.py
@@ -2,25 +2,24 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 W = np.array([
-    [10, 2, 5, 0, 0],
-    [0, 0, 5, 0, 0],
-    [5, 0, 0, 3, 0],
-    [5, 0, 0, 0, 3]
+    [10, 3, 2, 5, 0, 0],
+    [0, 0, 0, 5, 0, 0],
+    [5, -1, 0, 0, 3, 0],
+    [5, 0, 0, 0, 0, 3]
 ])
 
 df = pd.read_excel("data/Region_Estimation.xlsx",
                    sheet_name="standard", index_col="Region")
 data = df.values
 
-
 result = pd.DataFrame((W @ data.T).T, index=df.index,
-                      columns=["Y", "Y_In", "r_c", "r_p"])
+                        columns=["Y", "Y_In", "r_c", "r_p"])
 # print(result)
+
 nan_excel = pd.DataFrame()
 nan_excel.to_excel("result\Region_Estimaiton_Result.xlsx")
 writer = pd.ExcelWriter("result\Region_Estimaiton_Result.xlsx")
 result.to_excel(writer, sheet_name="pre")
-
 result = result.apply(lambda x: ((x-np.mean(x))/np.std(x)))
 # print(result)
 result.to_excel(writer, sheet_name="standard")
@@ -31,6 +30,7 @@ index = np.arange(3)
 error_config = {'ecolor': '0.3'}
 bar_width = 0.2
 fig, ax = plt.subplots()
+
 rect0 = ax.bar(index - 2 * bar_width, result["Y"], bar_width, alpha=0.5,
                color='r', yerr=0.1 * result["Y"].std(), error_kw=error_config, label="Y")
 
